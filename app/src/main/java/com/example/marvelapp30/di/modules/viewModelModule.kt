860cf1eb21af38b9e68566cac1669c73eb2a22f6
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-06-21 04:45:28 -0300

    vm module updated

--- app/src/main/java/com/example/marvelapp30/di/modules/viewModelModule.kt
+++ app/src/main/java/com/example/marvelapp30/di/modules/viewModelModule.kt
@@ -1,10 +1,11 @@
 package com.example.marvelapp30.di.modules
 
+import com.example.marvelapp30.featur_event.presentation.EventViewModel
 import com.example.marvelapp30.feature_character.presentation.CharacterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::CharacterViewModel)
-
+    viewModelOf(::EventViewModel)
 }
