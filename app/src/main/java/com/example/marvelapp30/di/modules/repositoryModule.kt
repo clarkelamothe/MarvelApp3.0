ad7a382316ea7284ad5b513873be0f060fdff81c
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-06-26 15:30:07 -0300

    di updated with event stuff

--- app/src/main/java/com/example/marvelapp30/di/modules/repositoryModule.kt
+++ app/src/main/java/com/example/marvelapp30/di/modules/repositoryModule.kt
@@ -3,6 +3,8 @@ package com.example.marvelapp30.di.modules
 import com.example.marvelapp30.feature_character.data.CharacterRepositoryImpl
 import com.example.marvelapp30.feature_character.data.paging.CharacterRemoteMediator
 import com.example.marvelapp30.feature_character.domain.CharacterRepository
+import com.example.marvelapp30.feature_event.data.EventRepositoryImpl
+import com.example.marvelapp30.feature_event.domain.EventRepository
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
@@ -10,4 +12,6 @@ import org.koin.dsl.module
 val repositoryModule = module {
     factoryOf(::CharacterRepositoryImpl) { bind<CharacterRepository>() }
     factory { CharacterRemoteMediator(get(), get()) }
+
+    factoryOf(::EventRepositoryImpl) { bind<EventRepository>() }
 }
--- app/src/main/java/com/example/marvelapp30/di/modules/serviceModule.kt
+++ app/src/main/java/com/example/marvelapp30/di/modules/serviceModule.kt
@@ -2,6 +2,8 @@ package com.example.marvelapp30.di.modules
 
 import com.example.marvelapp30.feature_character.data.remote.CharacterApi
 import com.example.marvelapp30.feature_character.data.remote.CharacterService
+import com.example.marvelapp30.feature_event.data.remote.EventApi
+import com.example.marvelapp30.feature_event.data.remote.EventService
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
@@ -9,6 +11,12 @@ val serviceModule = module {
     factory {
         CharacterService(
             get<Retrofit>().create(CharacterApi::class.java)
+        )
+    }
+
+    factory {
+        EventService(
+            get<Retrofit>().create(EventApi::class.java)
         )
     }
 }
