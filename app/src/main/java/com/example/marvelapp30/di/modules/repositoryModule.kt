369632b0c99b1024e644551fbbeaebae499f7acc
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-06-28 21:15:12 -0300

    rename in imports

--- app/src/main/java/com/example/marvelapp30/di/modules/repositoryModule.kt
+++ app/src/main/java/com/example/marvelapp30/di/modules/repositoryModule.kt
@@ -3,6 +3,8 @@ package com.example.marvelapp30.di.modules
 import com.example.marvelapp30.feature_character.data.CharacterRepositoryImpl
 import com.example.marvelapp30.feature_character.data.paging.CharacterRemoteMediator
 import com.example.marvelapp30.feature_character.domain.CharacterRepository
+import com.example.marvelapp30.feature_comic.data.ComicRepositoryImpl
+import com.example.marvelapp30.feature_comic.domain.ComicRepository
 import com.example.marvelapp30.feature_event.data.EventRepositoryImpl
 import com.example.marvelapp30.feature_event.domain.EventRepository
 import org.koin.core.module.dsl.bind
@@ -14,4 +16,6 @@ val repositoryModule = module {
     factory { CharacterRemoteMediator(get(), get()) }
 
     factoryOf(::EventRepositoryImpl) { bind<EventRepository>() }
+
+    factoryOf(::ComicRepositoryImpl) { bind<ComicRepository>() }
 }
--- app/src/main/java/com/example/marvelapp30/di/modules/serviceModule.kt
+++ app/src/main/java/com/example/marvelapp30/di/modules/serviceModule.kt
@@ -2,8 +2,8 @@ package com.example.marvelapp30.di.modules
 
 import com.example.marvelapp30.feature_character.data.remote.CharacterApi
 import com.example.marvelapp30.feature_character.data.remote.CharacterService
-import com.example.marvelapp30.feature_comics.data.remote.ComicApi
-import com.example.marvelapp30.feature_comics.data.remote.ComicService
+import com.example.marvelapp30.feature_comic.data.remote.ComicApi
+import com.example.marvelapp30.feature_comic.data.remote.ComicService
 import com.example.marvelapp30.feature_event.data.remote.EventApi
 import com.example.marvelapp30.feature_event.data.remote.EventService
 import org.koin.dsl.module
