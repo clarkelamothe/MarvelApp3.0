e727a6ee2c8dee450b76ae9e30b037d918e5ca89
Clarke Lamothe <clamothe@intermediait.com>
2023-07-03 12:38:59 -0300

    more error check

--- app/src/main/java/com/example/marvelapp30/feature_character/presentation/CharacterFragment.kt
+++ app/src/main/java/com/example/marvelapp30/feature_character/presentation/CharacterFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -80,6 +81,10 @@ class CharacterFragment : Fragment() {
                                 Snackbar.LENGTH_INDEFINITE
                             ).setAction("RETRY") { characterAdapter.refresh() }.show()
                         }
+                    }
+
+                    else -> {
+                        Toast.makeText(context, "Something happened!", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
