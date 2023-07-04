62741969ba1b0dbfecd706ee53bd43f30cd76977
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-07-04 14:13:06 -0300

    comics service added

--- app/src/main/java/com/example/marvelapp30/feature_character/data/remote/CharacterApi.kt
+++ app/src/main/java/com/example/marvelapp30/feature_character/data/remote/CharacterApi.kt
@@ -1,9 +1,11 @@
 package com.example.marvelapp30.feature_character.data.remote
 
+import com.example.marvelapp30.apiModel.ComicDto
 import com.example.marvelapp30.apiModel.Data
 import com.example.marvelapp30.apiModel.MarvelResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharacterApi {
@@ -12,4 +14,9 @@ interface CharacterApi {
         @Query("limit") limit: Int,
         @Query("offset") offset: Int
     ): Response<MarvelResponse<Data<List<CharacterDto>>>>
+
+    @GET("characters/{id}/comics")
+    suspend fun getComicsByCharacterId(
+        @Path("id") id: Int
+    ): Response<MarvelResponse<Data<List<ComicDto>>>>
 }
--- app/src/main/java/com/example/marvelapp30/feature_character/data/remote/CharacterService.kt
+++ app/src/main/java/com/example/marvelapp30/feature_character/data/remote/CharacterService.kt
@@ -6,4 +6,8 @@ class CharacterService(
     suspend fun getRemoteCharacters(
         limit: Int, offset: Int
     ) = characterApi.getCharacters(limit, offset)
+
+    suspend fun getRemoteComicsByCharacterId(
+        id: Int
+    ) = characterApi.getComicsByCharacterId(id)
 }
