371305ee7c63e5c3bb1ae4f77154b298525aed4d
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-08-26 16:44:17 -0300

    tiny fix

--- app/src/main/java/com/example/marvelapp30/utils/MarginItemDecorator.kt
+++ app/src/main/java/com/example/marvelapp30/utils/MarginItemDecorator.kt
@@ -4,7 +4,7 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class MarginItemDecorator() : RecyclerView.ItemDecoration() {
+class MarginItemDecorator: RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(
         outRect: Rect,
