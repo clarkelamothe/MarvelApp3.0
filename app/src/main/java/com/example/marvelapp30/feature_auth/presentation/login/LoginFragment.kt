7e66730f105a0dd4502dc02fd614e79f54c683eb
Clarke Lamothe <clarkelamothe@yahoo.fr>
2023-08-27 02:20:14 -0300

    signup and login

--- app/src/main/java/com/example/marvelapp30/feature_auth/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/marvelapp30/feature_auth/presentation/login/LoginFragment.kt
@@ -1,18 +1,34 @@
 package com.example.marvelapp30.feature_auth.presentation.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.example.marvelapp30.databinding.FragmentLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private var binding: FragmentLoginBinding? = null
+    private var auth = Firebase.auth
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            Log.d("FirebaseUser", "onStart: ${currentUser.displayName}")
+            goTo(LoginFragmentDirections.goToCharacters())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,11 +48,28 @@ class LoginFragment : Fragment() {
 
     private fun setListeners() {
         binding?.btLogin?.setOnClickListener {
-            goTo(LoginFragmentDirections.goToCharacters())
+            login(
+                binding?.etEmail?.text.toString(),
+                binding?.etPassword?.text.toString()
+            )
         }
 
         binding?.tvSignup?.setOnClickListener {
             goTo(LoginFragmentDirections.goToSignup())
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        if (email.isNotBlank() && password.isNotBlank()) {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(context, "Success!", Toast.LENGTH_SHORT).show()
+                        goTo(LoginFragmentDirections.goToCharacters())
+                    } else {
+                        Toast.makeText(context, "Failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 
--- app/src/main/java/com/example/marvelapp30/feature_auth/presentation/signup/SignupFragment.kt
+++ app/src/main/java/com/example/marvelapp30/feature_auth/presentation/signup/SignupFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.marvelapp30.databinding.FragmentSignupBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignupFragment : Fragment() {
     private var binding: FragmentSignupBinding? = null
+    private var auth = Firebase.auth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +38,24 @@ class SignupFragment : Fragment() {
         }
 
         binding?.btSignup?.setOnClickListener {
-            findNavController().popBackStack()
+            signup(
+                binding?.etEmail?.text.toString(),
+                binding?.etPassword?.text.toString()
+            )
+        }
+    }
+
+    private fun signup(email: String, password: String) {
+        if (email.isNotBlank() && password.isNotBlank()) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(context, "Success!", Toast.LENGTH_SHORT).show()
+                        findNavController().popBackStack()
+                    } else {
+                        Toast.makeText(context, "Failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 
